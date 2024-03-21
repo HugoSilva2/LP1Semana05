fbe76d8fdf6067e4d85b7dd4c6875cc497662055
HugoSilva2 <hugomiguelsarmentodasilva@gmail.com>
2024-03-21 23:22:00 +0000

    Random Die Done

--- RanDice/Program.cs
+++ RanDice/Program.cs
@@ -6,7 +6,41 @@ namespace RanDice
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+           if (args.Length != 2)  //if args length different than 2
+            {
+                Console.WriteLine("Usage: RanDice <number_of_dice> <seed>");
+                return;
+            }
+
+            // Parsing command line arguments
+            int numberOfDice, seed;
+            try
+            {
+                numberOfDice = int.Parse(args[0]);
+                seed = int.Parse(args[1]);
+            }
+            catch (FormatException) //To check if the format of an argument is invalid
+            {
+                Console.WriteLine("Invalid input format. Please enter integers for number of dice and seed.");
+                return;
+            }
+
+            if (numberOfDice <= 0)
+            {
+                Console.WriteLine("Invalid number of dice. Please enter a positive integer.");
+                return;
+            }
+
+            Random rand = new Random(seed);
+
+            int sum = 0;
+            for (int i = 0; i < numberOfDice; i++)
+            {
+                int roll = rand.Next(1, 7); // Generates a random number between 1 and 6 (inclusive)
+                sum += roll;
+            }
+
+            Console.WriteLine($"Sum of die: {sum}");
         }
     }
 }
