e38d31b9c5e6a6dd72e0fea285c9890d4a229dee
HugoSilva2 <hugomiguelsarmentodasilva@gmail.com>
2024-03-21 22:40:46 +0000

    Setting Up

---
+++ Chronos/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace Chronos
+{
+    class Program
+    {
+        //define both cronos
+        Stopwatch crono1 = new Stopwatch();
+        Stopwatch crono2 = new Stopwatch();
+
+        //Start crono1
+        
+        
+        //Stop crono1 after 0.6 seconds
+        
+
+        //Start crono2
+        
+        //Stop crono2 after 0.2 seconds
+
+        //Stop Both cronos
+
+
+        //Get the both times in seconds
+
+        //Print
+    }
+}
+
